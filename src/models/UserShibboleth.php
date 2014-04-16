8f3cd151ec7d30166c602e77d31bee7a815e483e
Michael <michaeljs1990@gmail.com>
2014-04-16 15:08:02 -0500

    Security update.

--- src/models/UserShibboleth.php
+++ src/models/UserShibboleth.php
@@ -34,6 +34,37 @@ class UserShibboleth extends Eloquent implements UserInterface, RemindableInterf
 	}
 
 	/**
+	 * Get the token value for the "remember me" session.
+	 *
+	 * @return string
+	 */
+	public function getRememberToken()
+	{
+		return $this->remember_token;
+	}
+
+	/**
+	 * Set the token value for the "remember me" session.
+	 *
+	 * @param  string  $value
+	 * @return void
+	 */
+	public function setRememberToken($value)
+	{
+		$this->remember_token = $value;
+	}
+
+	/**
+	 * Get the column name for the "remember me" token.
+	 *
+	 * @return string
+	 */
+	public function getRememberTokenName()
+	{
+		return 'remember_token';
+	}
+
+	/**
 	 * Get the password for the user.
 	 *
 	 * @return string
